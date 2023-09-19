8dd8c42e55071e9e2722b63443a33946e1501863
Dwight VanTuyl <dvantuyl@gmail.com>
2023-09-19 05:24:48 -0700

    Emily 2023-09-21 edits

--- src/components/MoreContainer.svelte
+++ src/components/MoreContainer.svelte
@@ -2,15 +2,17 @@
   let viewingMore = false;
 
   function viewMore() {
-   viewingMore = true; 
+    viewingMore = true;
   }
-
 </script>
 
-<div class="relative sm:max-h-full overflow-hidden"
+<div
+  class="relative sm:max-h-full overflow-hidden"
   class:max-h-[20rem]={!viewingMore}
 >
-  <slot />
+  <div class="flex flex-col gap-3 sm:gap-6">
+    <slot />
+  </div>
   {#if !viewingMore}
     <div
       class="block sm:hidden absolute bottom-0 left-0 right-0 h-[6rem] bg-gradient-to-t from-secondary-300 to-transparent"
@@ -19,10 +21,10 @@
 </div>
 <div class="block sm:hidden bg-secondary-300 opacity-90">
   {#if !viewingMore}
-  <button
-    on:click={viewMore}
-    class="block text-center text-base-700 text-stroke-1 font-bold mt-4 mx-auto"
-    >more &blacktriangledown;</button
-  >
+    <button
+      on:click={viewMore}
+      class="block text-center text-base-700 text-stroke-1 font-bold mt-4 mx-auto"
+      >more &blacktriangledown;</button
+    >
   {/if}
 </div>
