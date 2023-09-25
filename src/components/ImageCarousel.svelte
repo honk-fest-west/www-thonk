bd935c853ae4e097512519fb12cbb6e5cfa127f4
Dwight VanTuyl <dvantuyl@gmail.com>
2023-09-24 20:04:20 -0700

    Image Carousel

---
+++ src/components/ImageCarousel.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { Splide, SplideSlide } from "@splidejs/svelte-splide";
+  import "@splidejs/svelte-splide/css";
+  export let slides;
+</script>
+
+<Splide
+  options={{ type: "loop", autoplay: true, interval: 3000 }}
+  aria-label="HONK! Fest West 2022"
+>
+  {#each slides as { img: { src, width, height }, alt }}
+    <SplideSlide>
+      <img {src} {alt} class="max-h-[36rem]" />
+    </SplideSlide>
+  {/each}
+</Splide>
+
+<style>
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+</style>
